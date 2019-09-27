dbc46e3deb68cdde11affa7d1295328c2623bb03
Blake Trapnell <Blake.Trapnell103@gmail.com>
2019-09-27 13:23:22 -0600

    additional home page styling

--- db/seed.sql
+++ db/seed.sql
@@ -1,5 +1,6 @@
 DROP TABLE users;
-DROP TABLE promo_codes
+DROP TABLE promo_codes;
+DROP TABLE skills;
 
 CREATE TABLE users (
 user_id SERIAL PRIMARY KEY,
@@ -12,3 +13,26 @@ Hash TEXT
 );
 
 CREATE TABLE promo_codes (promo_id SERIAL PRIMARY KEY, promo_code TEXT, promo_time TEXT);
+
+CREATE TABLE skills (
+skills_id SERIAL PRIMARY KEY,
+sheet_id INT,
+user_id INT,
+acrobatics BOOLEAN,
+arcana BOOLEAN,
+athletics BOOLEAN,
+deception BOOLEAN,
+history BOOLEAN,
+insight BOOLEAN,
+intimidation BOOLEAN,
+investigation BOOLEAN,
+medicine BOOLEAN,
+nature BOOLEAN,
+perception BOOLEAN,
+performance BOOLEAN,
+persuassion BOOLEAN,
+religion BOOLEAN,
+sleight_of_hand BOOLEAN,
+stealth BOOLEAN,
+Survival BOOLEAN
+);
--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -9,13 +9,37 @@ export default function Home() {
             <div className="help-notifications">   
                 <div className="home-notifications"></div>
                 <div className="home-help">
-                    <button className="home-help-button">Help</button>
+                    {/* <button className="home-logout-button">out</button>
+                    <button className="home-help-button">Help</button> */}
                     
                     </div>    
             </div>
             <div className="home-main">
-                <div className="home-navigation"></div>
-                <div className="home-main-display"></div>
+                <div className="home-navigation">
+                    <div className="adventure-league">
+                        <h1 >A.L.</h1>
+                        <h6 className="navigation-subtext">Adventure <p>League</p></h6>
+                    </div>
+                    <div className="random-creator">
+                        <h1>R.C</h1>
+                        <h6 className="navigation-subtext">Random <p>Creator</p></h6>
+                    </div>
+                    <div className="custom-create">
+                        <h1>C.C</h1>
+                        <h6 className="navigation-subtext">Custom <p>Create</p></h6>
+                    </div>
+                </div>
+                <div className="home-main-display">
+                    <div className="home-sheets-display"></div>
+                    <div className="fun-fact">
+                        <div className="did-you-know">
+                        <h1>Did you know?</h1>
+                        </div>
+                        <div className="home-information">
+                        <h4>Barbarians use Strength as their main stat</h4>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
     )
